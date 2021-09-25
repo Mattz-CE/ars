c8dc4b141f5eb02993cbf297c16d18a6bd3fae64
Matt <61751438+Mattz-CE@users.noreply.github.com>
2021-09-26 05:55:26 +0900

    Update ars.py

--- ars/ars.py
+++ ars/ars.py
@@ -8,20 +8,6 @@ import aiohttp
 import discord
 from redbot.core import commands
 
-try:
-    import cairosvg
-
-    svg_convert = "cairo"
-except:
-    try:
-        from wand.image import Image
-
-        svg_convert = "wand"
-    except:
-        svg_convert = None
-
-log = logging.getLogger("red.ars.ars")
-
 
 class Ars(commands.Cog):
 
@@ -30,13 +16,6 @@ class Ars(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.session = aiohttp.ClientSession()
-        if svg_convert == "cairo":
-            log.debug("Ars.py: Using CairoSVG for svg conversion.")
-        elif svg_convert == "wand":
-            log.debug("Ars.py: Using wand for svg conversion.")
-        else:
-            log.debug(
-                "Ars.py: Failed to import svg converter. Standard emoji will be limited to 72x72 png.")
 
     def cog_unload(self):
         self.bot.loop.create_task(self.session.close())
@@ -46,7 +25,7 @@ class Ars(commands.Cog):
         return
 
     @commands.command(name="fuck")
-    async def fuck(self, ctx, emoji):
+    async def fuck(self, ctx):
         """Fuck (/fʌk/ fuhk) is a profane English-language word."""
         channel = ctx.channel
         convert = False
